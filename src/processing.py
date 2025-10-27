ef987f2fe760c0a632c5c8740a3e2f276ac6d97c
Konstantin Botnev <konstantinbotnev0712@gmai.com>
2025-10-27 10:53:04 +0300

    удалил файл idea

--- src/processing.py
+++ src/processing.py
@@ -8,8 +8,8 @@ def filter_by_state(banking_operation: List[Dict[str, str]], state="EXECUTED") -
     return [x for x in banking_operation if x["state"] == state]
 
 
-def sort_by_date(banking_operation: List[Dict[str, str]], reverse: bool = True) -> List[Dict[str, str]]:
+def sort_by_date(banking_operation: List[Dict[str, str]]) -> List[Dict[str, str]]:
     """Функция принимает на вход список словарей и параметр порядка
     сортировки, возвращает новый список, в котором исходные словари
     отсортированы по дате"""
-    return sorted(banking_operation, key=lambda x: datetime.fromisoformat(x["date"]), reverse=True)
+    return sorted(banking_operation, key=lambda x: datetime.fromisoformat(x["date"]), reverse=False)
