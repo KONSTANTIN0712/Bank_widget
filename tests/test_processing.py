af4a7af4259f1dca28baa7e3831d4dbea43c7b3a
Konstantin Botnev <konstantinbotnev0712@gmai.com>
2025-12-30 01:22:51 +0300

    change file readme

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -24,7 +24,7 @@ def test_filter_by_state_no_state() -> None:
         'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
 
 def test_sort_by_date() -> None:
-    """Тест функции работающей по дате"""
+    """Тест функции сортировки по дате"""
     assert sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
                             {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
                             {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
@@ -35,5 +35,5 @@ def test_sort_by_date() -> None:
         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
 
 def test_sort_date_non_date(sort_date_none) ->None:
-    """Тест функции с отсутствием даты"""
+    """Тест функции сортировки с отсутствием даты"""
     assert sort_by_date(sort_date_none) == "Отсутствует дата"
